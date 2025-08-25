5aca9084b3a55af613395020a6d062c46280ad83
Armless Master <dima.shevchenkooooo@gmail.com>
2025-08-25 17:08:21 +0300

    added choose path button which opens default-windows window to choose target path

---
+++ Source/AutoVersionIncrementer/Private/AVI_PackageAndIncrementEditorUtilityWidget.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AVI_PackageAndIncrementEditorUtilityWidget.h"
+#include "DesktopPlatformModule.h"
+
+void UAVI_PackageAndIncrementEditorUtilityWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	ChoosePathButton->OnClicked.AddDynamic(this, &ThisClass::OnChoosePathButtonPressed);
+}
+
+void UAVI_PackageAndIncrementEditorUtilityWidget::OnChoosePathButtonPressed()
+{
+	if (IDesktopPlatform* DesktopPlatform = FDesktopPlatformModule::Get())
+	{
+		const void* ParentWindowHandle = FSlateApplication::Get().FindBestParentWindowHandleForDialogs(nullptr);
+
+		FString OutFolderName;
+		const FString Title = TEXT("Select a folder for packaging");
+		const FString DefaultPath = FPaths::ProjectDir();
+
+		const bool bOpened = DesktopPlatform->OpenDirectoryDialog(
+			ParentWindowHandle,
+			Title,
+			DefaultPath,
+			OutFolderName
+		);
+
+		if (bOpened)
+		{
+			PackagePathTextBox->SetText(FText::FromString(OutFolderName));
+		}
+	}
+}
---
+++ Source/AutoVersionIncrementer/Public/AVI_PackageAndIncrementEditorUtilityWidget.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorUtilityWidget.h"
+#include "EditorUtilityWidgetComponents.h"
+#include "AVI_PackageAndIncrementEditorUtilityWidget.generated.h"
+
+UCLASS()
+class AUTOVERSIONINCREMENTER_API UAVI_PackageAndIncrementEditorUtilityWidget : public UEditorUtilityWidget
+{
+	GENERATED_BODY()
+
+protected:
+	UFUNCTION()
+	void OnChoosePathButtonPressed();
+
+	virtual void NativeConstruct() override;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UEditorUtilityButton> ChoosePathButton;
+	
+	UPROPERTY(meta = (BindWidget), BlueprintReadWrite)
+	TObjectPtr<UEditorUtilityEditableTextBox> PackagePathTextBox;
+};
